feb65902322f4d2eafbfe94ab0cd9cf7a013bf9c
Biitopher <biitopher@gmail.com>
2024-01-04 22:25:32 +0000

    Task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Returns integers list"""
+    if n <= 0:
+        return []
+    
+    pascal_triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j]
+        pascal_triangle.append(row)
+    
+    return pascal_triangle
