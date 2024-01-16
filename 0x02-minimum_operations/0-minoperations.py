46375c9e11dd25d8b1a863d8ab796019aa902188
Biitopher <biitopher@gmail.com>
2024-01-16 09:36:32 +0000

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,33 +4,14 @@
 
 def minOperations(n):
     """defines minimum operations"""
-    if n <= 1:
+    if n < 2:
         return 0
-
-    def prime_factors(num):
-        """defines prime factors"""
-        factors = []
-        divisor = 2
-        while divisor * divisor <= num:
-            while (num % divisor) == 0:
-                factors.append(divisor)
-                num //= divisor
-            divisor += 1
-        if num > 1:
-            factors.append(num)
-        return factors
-
-    factors = prime_factors(n)
-
-    if not factors:
-        return 0
-
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for factor in factors:
-            if i % factor == 0:
-                dp[i] = min(dp[i], dp[i // factor] + factor)
-
-    return dp[n]
+    factor_list = []
+    x = 1
+    while n != 1:
+        x += 1
+        if n % x == 0:
+            while n % x == 0:
+                n /= x
+                factor_list.append(x)
+    return sum(factor_list)
