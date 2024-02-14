2821549030db9448e602b44ea71b3a4bb07143b5
biitopher <biitopher@gmail.com>
2024-02-14 00:10:27 -0800

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,22 @@
 #!/usr/bin/node
 
-import requests
-import sys
+const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-def get_movie_characters(movie_id):
-    base_url = "https://swapi.dev/api/"
-
-    film_url = f"{base_url}films/{movie_id}/"
-    film_response = requests.get(film_url)
-    film_data = film_response.json()
-
-    character_urls = film_data.get('characters', [])
-
-    characters = []
-    for character_url in character_urls:
-        character_response = requests.get(character_url)
-        character_data = character_response.json()
-        characters.append(character_data['name'])
-
-    return characters
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <Movie ID>")
-        sys.exit(1)
-
-    movie_id = sys.argv[1]
-
-    try:
-        characters = get_movie_characters(movie_id)
-        print(f"Characters in Star Wars Episode {movie_id}:")
-        for character in characters:
-            print(character)
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
