ddb20a55e95039c5d7207652cd638078ee77fb0b
biitopher <biitopher@gmail.com>
2024-02-29 22:15:37 +0000

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,15 +7,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-
-    dp[0] = 0
+    coins.sort()
+    dp = [0] + [float('inf')] * total
 
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    if dp[total] == float('inf'):
-        return -1
-
-    return dp[total]
+    return dp[total] if dp[total] != float('inf') else -1
